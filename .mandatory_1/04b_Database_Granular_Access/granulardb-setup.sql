fb1c31c29a0eb3890002f880567819ef58c4d068
Rashaun Godding <90447626+rgodding@users.noreply.github.com>
2024-03-21 00:06:52 +0100

    commit

--- .mandatory_1/04b_Database_Granular_Access/granulardb-setup.sql
+++ .mandatory_1/04b_Database_Granular_Access/granulardb-setup.sql
@@ -64,28 +64,3 @@ CREATE POLICY items_policy_manager ON gs.items
 FOR ALL
 TO manager
 USING (true);
-
--- Drop  All
--- POLICIES
-DROP POLICY items_policy_customer ON gs.items;
-DROP POLICY items_policy_employee ON gs.items;
-DROP POLICY items_policy_manager ON gs.items;
--- ROW LEVEL SECURITY
-ALTER TABLE gs.items DISABLE ROW LEVEL SECURITY;
-
--- ROLES
-REVOKE ALL ON SCHEMA gs FROM customer;
-REVOKE ALL ON DATABASE testdb FROM customer;
-REVOKE ALL ON SCHEMA gs FROM employee;
-REVOKE ALL ON DATABASE testdb FROM employee;
-REVOKE ALL ON SCHEMA gs FROM manager;
-REVOKE ALL ON DATABASE testdb FROM manager;
-DROP ROLE customer;
-DROP ROLE employee;
-DROP ROLE manager;
-
--- TABLES
-DROP TABLE gs.items;
-
--- SCHEMAS
-DROP SCHEMA gs;
